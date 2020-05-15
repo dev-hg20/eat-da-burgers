031893d6f6c7f510bc543e0383875f323b73a4c7
Haylie Goh <h.goh@deakin.edu.au>
2020-05-15 18:05:00 +1000

    partials folder included

---
+++ public/assets/js/burger.js
@@ -0,0 +1,35 @@
+$(function () {
+  $(".devoured-button").on("click", function (e) {
+    let id = $(this).data("id");
+    const newDevoured = $(this).data("newburger");
+    const devouredState = {
+      devoured: newDevoured,
+    };
+
+    //PUT request
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: devouredState,
+    }).then(function () {
+      console.log("This has now been eaten");
+      //load uploaded list
+      location.reload();
+    });
+  });
+
+  $(".form").on("submit", function (e) {
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#burgerInput").val().trim(),
+    };
+    //POST request
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger,
+    }).then(function () {
+      console.log("new burger has been created");
+      location.reload();
+    });
+  });
+});
